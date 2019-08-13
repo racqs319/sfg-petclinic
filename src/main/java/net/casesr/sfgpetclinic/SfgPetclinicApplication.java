d77b585343c228b6a1728ad8cfc3f6c64442f2d3
racqs319 <racquel.cases@hotmail.com>
2019-08-13 18:09:17 +0800

    Initial Project Add

---
+++ src/main/java/net/casesr/sfgpetclinic/SfgPetclinicApplication.java
@@ -0,0 +1,13 @@
+package net.casesr.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetclinicApplication.class, args);
+    }
+
+}
