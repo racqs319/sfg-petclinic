6c984903a2d8324a963478e23f46f3398fc44aa2
racqs319 <racquel.cases@hotmail.com>
2019-08-18 15:34:35 +0800

    Add ID to model objects. Closes #10.

--- pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package net.casesr.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastNme;
 
--- pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package net.casesr.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
