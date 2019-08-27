7ff34de5f55f7247839644510bd238cdf570ea1b
Racquel Cases <racquel.cases@hotmail.com>
2019-08-27 16:49:52 +0800

    Create PetType map service. Closes #29.

---
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package net.casesr.sfgpetclinic.services;
+
+import net.casesr.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,39 @@
+package net.casesr.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import net.casesr.sfgpetclinic.model.PetType;
+import net.casesr.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> 
+		implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
