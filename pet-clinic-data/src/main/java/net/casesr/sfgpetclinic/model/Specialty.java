e5c3aa8ba73fbfa0db5581caa09bed67b5719f05
Racquel Cases <racquel.cases@hotmail.com>
2019-08-27 16:35:06 +0800

    Create Specialty entity and add to Vets. Closes #27.

---
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,15 @@
+package net.casesr.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity {
+	
+	private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setescription(String description) {
+        this.description = description;
+    }
+
+}
