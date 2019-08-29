686a62c915f8700c7ac28c7c9c1016c91e2c2441
Racquel Cases <racquel.cases@hotmail.com>
2019-08-29 11:21:13 +0800

    Implement JPA BaseEntity. Closes #35.

--- pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/BaseEntity.java
@@ -2,9 +2,17 @@ package net.casesr.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
     public Long getId() {
         return id;
