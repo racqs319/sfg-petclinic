00074ab4635353e5dc42f0e2476246f59eb3cb9a
Racquel Cases <racquel.cases@hotmail.com>
2019-08-29 22:05:19 +0800

    Add Visit map service. Closes #46.

---
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package net.casesr.sfgpetclinic.services;
+
+import net.casesr.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package net.casesr.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import net.casesr.sfgpetclinic.model.Visit;
+import net.casesr.sfgpetclinic.repositories.VisitRepository;
+import net.casesr.sfgpetclinic.services.VisitService;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> 
+		implements VisitService {
+	
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit object) {
+		if(object.getPet() == null || object.getPet().getOwner() == null
+				|| object.getPet().getId() == null || object.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid visit");
+		}
+		
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
