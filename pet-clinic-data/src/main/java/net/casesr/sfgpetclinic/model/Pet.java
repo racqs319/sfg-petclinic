90d0bed6afc780a320c41065b9bc71621514bb61
Racquel Cases <racquel.cases@hotmail.com>
2019-10-08 13:11:08 +0800

    Fix saving of new or updated Pet. Closes #65.

--- pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/net/casesr/sfgpetclinic/model/Pet.java
@@ -17,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -48,6 +49,7 @@ public class Pet extends BaseEntity {
     private Owner owner;
     
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
     
     @Column(name = "name")
